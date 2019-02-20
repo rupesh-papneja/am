c6c5aabedf3496d2c38289bf4665b8ebc4f8b212
Rupesh Papneja <rupesh.papneja@gmail.com>
2019-02-20 18:21:28 +1100

    Correcting the imports for Rule service

--- rules/com.am.service/src/main/java/com/am/service/RulesRestController.java
+++ rules/com.am.service/src/main/java/com/am/service/RulesRestController.java
@@ -1,12 +1,9 @@
 package com.am.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class RulesRestController {
